8c18228be96b5d52859423995bd55a0b9e52e397
eduardocuapio <lalodcuapio@gmail.com>
2022-09-28 20:26:36 -0500

    ProyetoC

---
+++ CYP02/CYP02.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(void)
+{
+	//piquense el culo culeros xd
+	int Posi, Tm, PosiH, TmH, Cer1, Cer2;
+	Cer1=0000, 
+	Cer2=0000;
+	scanf_s("%i", &Tm);
+	scanf_s("%i", &Posi);
+	TmH=Tm;
+	PosiH=Posi;
+	printf("%c%c%08X%04i%04i%08X", 66, 77, TmH, Cer1, Cer2, PosiH);
+}
